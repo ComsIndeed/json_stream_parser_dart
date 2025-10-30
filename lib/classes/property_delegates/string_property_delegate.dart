9632f5d41a4eed9918e42a305309cdf44ecf29de
Coms <125128463+ComsIndeed@users.noreply.github.com>
2025-10-31 00:30:29 +0800

    refactor: complete string handling in StringPropertyDelegate by finalizing property stream on chunk end

--- lib/classes/property_delegates/string_property_delegate.dart
+++ lib/classes/property_delegates/string_property_delegate.dart
@@ -29,8 +29,9 @@ class StringPropertyDelegate extends PropertyDelegate {
     if (character == '"') {
       isDone = true;
       addPropertyChunk(_buffer);
-      // TODO : call finish for string streams
-
+      parserController
+          .getPropertyStreamController(propertyPath)
+          .complete("THIS VALUE IS IGNORED");
       return;
     }
     _buffer += character;
